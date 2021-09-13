1d5be4fafa22114bdd1e5d86d9b76a5d561d35df
Mykhailo Bondarchuk <bssoag@gmail.com>
2021-09-13 17:26:06 +0300

    equals() and hashcode() realisation for student

---
+++ HWStudent/src/main/java/Student.java
@@ -0,0 +1,17 @@
+public class Student {
+    String name;
+    int age;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return age == student.age && name.equals(student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return (31 * age + ((name == null) ? 0 : name.hashCode()));
+    }
+}
